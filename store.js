ed5496b1e03d385b401bd23086e2d4b972802e9d
Nick Farni <nickfarni001@gmail.com>
2020-01-23 17:19:24 -0700

    consolidated showcase images and started creating a STORE

---
+++ store.js
@@ -0,0 +1,20 @@
+const STORE = [{
+        showcaseImage: ["primaryShowcaseImage","",""],
+        yearCreated: 2020,
+        titleOfProject: "The Finest Pineapple Quiz",
+        techUsed: ["HTML", "CSS", "Javascript"],
+        description: "This quiz tests your knowledge of pineapples. It is administered by a loquacious pineapple. However incorrigible this fruit is, nay we forget its indescribable deliciousness. Assignment for Thinkful Bootcamp.",
+        liveLink: "https://nick-nack-attack.github.io/final-pineapple-quiz/",
+        repoLink: "https://github.com/nick-nack-attack/final-pineapple-quiz.git",
+    },
+    {
+        showcaseImage: [],
+        yearCreated: 2020,
+        titleOfProject: "Flee to a Charity",
+        techUsed: ["HTML", "CSS", "Javascript"],
+        description: "Find a charity on the opposite side of the planet. Why? To abandon all your responsibilities and loved ones to help people you've never seen in a place you've never been to before, that's why!",
+        liveLink: "https://nick-nack-attack.github.io/flee-to-a-charity/",
+        repoLink: "https://github.com/nick-nack-attack/flee-to-a-charity.git",
+
+    }
+]
