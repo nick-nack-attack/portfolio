7b456bdc2d1fc7bdfa31a9ae24874055e7d67885
Nick Farni <nickfarni001@gmail.com>
2020-06-26 21:27:50 -0600

    whoops should be on a branch. added two new projects

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 'use strict'
 
+// check the current user time to get greeting
 function checkTime() {
     const now = new Date().getHours();
     determineTime(now);
-}
+};
 
+// func to find out the time is in 24 hr day (i.e. '17' is '5:00pm')
+// use this to determine proper greeting to use
 function determineTime(now) {
-    console.log(now);
     if (21 < now && now <= 24) {
         $('#period-of-day').text('Night');
+        enableDarkMode();
     } else if (0 < now && now <= 4) {
         $('#period-of-day').text('Night');
-    } else if (4 < now && now <= 12 ) {
+        enableDarkMode();
+    } else if (4 < now && now <= 12) {
         $('#period-of-day').text('Morning');
     } else if (12 < now && now <= 18) {
         $('#period-of-day').text('Afternoon');
@@ -20,35 +24,223 @@ function determineTime(now) {
     } else {
         $('#period-of-day').text('Day!');
     }
-} 
+};
 
-function getEachProjectShowcase() {
-    const results =  [];
-    for (let i=0; i<STORE.length; i++) {
-        results.push(`
-            <div class="showcase">
-                    <img class="${STORE[i].showcaseImage[0]}" src="${STORE[i].showcaseImage[1]}" alt="${STORE[i].showcaseImage[2]}">
-                    <p class="showcase_captions">${STORE[i].yearCreated}</p>
-                <h3>${STORE[i].titleOfProject}</h3>
-                    <p class="showcase_captions showcase_captions_bottom">${STORE[i].techUsed.map(item => `${item} `).join('')}</p>
-                    <p class="item_description">${STORE[i].description}</p>
-            <div class="showcase_item_buttons_bar">
-                <button class="showcase_button" onclick="window.open('${STORE[i].liveLink}','_blank')">Live</button>
-                <button class="showcase_button" onclick="window.open('${STORE[i].repoLink}','_blank')">View on github</button>
+// fun to generate project showcase image
+function generateImage(img) { 
+    return `
+        <img 
+            class="${img.class}" 
+            src="${img.src}" 
+            alt="${img.alt}"
+        >`
+};
+
+// function to map through each tech used on a project
+function generateTechUsed(array) {
+    return array
+        .map(tech => `${tech} ` )
+        .join(' ')
+};
+
+// created func to generate all buttons attached to a project
+function generateButtons(array) {
+    return array    
+        .map(btn =>
+            `
+                <button
+                    class="showcase_button"
+                    onclick="window.open('${btn.link}','_blank')"
+                >
+                    ${btn.title}
+                </button>
+            `    
+        )
+        .join(' ')
+};
+
+// map through STORE and create each project showcase
+function generateShowcase(array) {
+    return array.map(project => 
+        `
+            <div id="${project.showcaseImage.id}" class="showcase">
+                    ${generateImage(project.showcaseImage)}
+                <div class="showcase_content">
+                    <div class="title_tech">
+                        <p class="showcase_captions">
+                            ${project.yearCreated}
+                        </p>
+                        <h3>
+                            ${project.titleOfProject}
+                        </h3>
+                        <p class="showcase_captions showcase_captions_bottom">
+                            ${generateTechUsed(project.techUsed)}
+                        </p>
+                    </div>
+                    <div class="desc_buttons">
+                        <p class="item_description">
+                            ${project.description}
+                        </p>
+                        <div class="showcase_item_buttons_bar">
+                            ${generateButtons(project.buttons)}
+                    </div>
+                    </div>
+                </div>
             </div>
-            </div>
-        `)
+        `
+    );
+};
+
+// make each project within the STORE
+function setShowcase() {
+    $('#projects-area').append(generateShowcase(STORE));
+};
+
+// Get the header
+let myWorkHeader = document.getElementById("myWorkNav");
+const sg = document.getElementById("SG");
+const tc = document.getElementById("TC"); 
+const rm = document.getElementById("RM");
+const pq = document.getElementById("PQ"); 
+
+$("#PQ").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#hero_pq").offset().top
+    }, 2000);
+    $("#PQ").addClass("active");
+});
+
+$("#RM").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#hero_rm").offset().top
+    }, 2000);
+});
+
+$("#TC").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#hero_tc").offset().top
+    }, 2000);
+});
+
+$("#SG").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#hero_sg").offset().top
+    }, 2000);
+    $("#sg_li").addClass("active");
+});
+
+$(window).on("scroll", function() {
+
+    let currentPos = $(window).scrollTop();
+
+    $('.nav li a').each(function() {
+
+        let sectionLink = $(this);
+        let section = $(sectionLink.attr('href'));
+
+            // if the top of the section is less than the current position
+            // or if the top of the link plus the section height is more than the current 
+        // if (section.position().top <= currentPos && sectionLink.offset().top + section.height() >= currentPos) {
+            if (section.position().top <= currentPos && section.position().top + section.height() >= currentPos) {
+            // $('.nav li').removeClass('active');
+            sectionLink.addClass('active');
+        } else {
+            sectionLink.removeClass('active');
+        }
+    })
+
+})
+
+
+
+
+// Get the offset position of the navbar
+let sticky = myWorkHeader.offsetTop;
+
+function handleMenu() {
+    if ( window.pageYOffset > sg_div ) {
+        sg.classList.add("active")
+      } else if ( window.pageYOffset > tc_div ) {
+          tc.classList.add("active")
+      } else if ( window.pageYOffset > rm_div ) {
+        rm.classList.add("active")
+    } else if ( window.pageYOffset > pq_div ) {
+        pq.classList.add("active")
+    } else ('');
+}
+
+// Add the sticky class to the header when you reach its 
+// scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+    if (window.pageYOffset > sticky) {
+        myWorkHeader.classList.add("sticky");
+    } else {
+        myWorkHeader.classList.remove("sticky");
     }
-    return results;
-}
+  }
 
-function generateShowcase() {
-    $('#projects-area').append(getEachProjectShowcase().join(''))
-}
+// makes everything ... Perfect(ly) Dark :-)
+function enableDarkMode() {
+    // for body
+    let element = document.body;
+    element.classList.toggle("dark-mode");
+};
+
+
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme) {
+    document
+        .documentElement
+        .setAttribute(
+            'data-theme', 
+            currentTheme
+        );
+    if (currentTheme === "dark") {
+        toggleSwitch.checked = true;
+    }
+};
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document
+            .documentElement
+            .setAttribute(
+                'data-theme', 'dark'
+            );
+            localStorage.setItem('theme', 'dark');
+            $('button').addClass('dark-button');
+            $('body').addClass('dark-mode');
+            $('a').addClass('dark')
+    }
+    else {
+        document
+            .documentElement
+            .setAttribute(
+                'data-theme', 'light'
+            );
+            localStorage.setItem('theme', 'light');
+            $('button').removeClass('dark-button');
+            $('body').removeClass('dark-mode');
+            $('a').addClass('dark')
+    }
+};
+
+toggleSwitch.addEventListener('change', switchTheme, false);
+
+
+
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {
+    myFunction();
+};
 
 function runPage() {
-    checkTime()
-    generateShowcase();
+    checkTime();
+    setShowcase();
 }
 
 runPage();
--- store.js
+++ store.js
@@ -1,28 +1,137 @@
-const STORE = [{
-        showcaseImage: ["primaryShowcaseImage","images/showcase_image_pineapple.jpg","image showing showcase of pineapple quiz"],
-        yearCreated: 2020,
-        titleOfProject: "The Finest Pineapple Quiz",
-        techUsed: ["HTML", "CSS", "Javascript"],
-        description: "I'm working my way through the Thinkful Software engineering bootcamp. One of the assignments was to create a quiz. Mine is administered by a loquacious pineapple. However incorrigible this fruit may be, nay we forget its indescribable deliciousness.",
-        liveLink: "https://nick-nack-attack.github.io/final-pineapple-quiz/",
-        repoLink: "https://github.com/nick-nack-attack/final-pineapple-quiz.git",
-    },
+const STORE = [
     {
-        showcaseImage: ["primaryShowcaseImage","images/showcase_image_flee.jpg","image showing showcase of charity api app"],
-        yearCreated: 2020,
-        titleOfProject: "Flee to a Charity",
-        techUsed: ["HTML", "CSS", "Javascript"],
-        description: "For an API assignement, find a charity on the opposite side of the planet. Why? To abandon all your responsibilities and loved ones to help people you've never seen in a place you've never been to before, that's why!",
-        liveLink: "https://nick-nack-attack.github.io/flee-to-a-charity/",
-        repoLink: "https://github.com/nick-nack-attack/flee-to-a-charity.git",
-    },
-    {
-        showcaseImage: ["primaryShowcaseImage","images/SG_showcase.jpg","image showing showcase of Sober Grid mobile app"],
+        // refactored showcase image to include key
+        showcaseImage: 
+            {
+                id: "hero_sg",
+                class: "primaryShowcaseImage",
+                src: "images/hero_sobergrid.jpg",
+                alt: "image showing showcase of Sober Grid mobile app"
+            },
         yearCreated: '2017-2019',
         titleOfProject: "Sober Grid",
-        techUsed: ["Swift", "Kotlin", "Ruby", "JIRA"],
+        techUsed: 
+            [
+                "Swift", 
+                "Kotlin", 
+                "Ruby", 
+                "AWS", 
+                "JIRA"
+            ],
         description: "Managed software development and create UI/UX for the company’s flaghsip iOS, Android, and web apps with overseas software engineers. Achieved 4.9/5 Apple App Store and 4.5/5 Google Play Store rating. First place finish in Robert Wood Johnson Foundation competition (out of 97). Included in Best Alcohol Addiction Recovery Apps of 2019 by Healthline.",
-        liveLink: "https://www.sobergrid.com/",
-        repoLink: "https://apps.apple.com/us/app/sober-grid-social-network/id912632260"
+        buttons: [
+            // refactored buttons to include title and link key
+            // and possible to insert n number of buttons
+            {
+                title: "View Live",
+                link: "https://www.sobergrid.com/"
+            }, 
+            {
+                title: "View on App Store",
+                link: "https://apps.apple.com/us/app/sober-grid-social-network/id912632260"
+            },
+            {
+                title: "View on Google Play Store",
+                link: "https://apps.apple.com/us/app/sober-grid-social-network/id912632260"
+            }
+        ]
+    },
+    {
+        showcaseImage: 
+            {
+                id: "hero_tc",
+                class: "primaryShowcaseImage",
+                src: "images/hero_truthchecker.jpg",
+                alt: "image showing showcase of Truth Checker web app"
+            },
+        yearCreated: '2020',
+        titleOfProject: 'Truth Checker',
+        techUsed: 
+            [
+                "HTML5", 
+                "CSS 3", 
+                "ReactJS", 
+                "Node.js", 
+                "Express", 
+                "PostgreSQL"
+            ],
+        description: "A parody database for the truth. This application is a promotion piece for a music album. Users can submit facts to be approved by the administrator. The admin login is purposely left public to allow for users to approved or disapprove their own facts.",
+        buttons: [
+            {
+                title: "View Live",
+                link: "https://truth-checker.vercel.app/"
+            },
+            {
+                title: "Server Repo",
+                link: "https://github.com/nick-nack-attack/truth-checker-api"
+            },
+            {
+                title: "Client Repo",
+                link: "https://github.com/nick-nack-attack/truth-checker-client"
+            }
+        ]
+    },
+    {
+        showcaseImage: 
+            {
+                id: "hero_rm",
+                class: "primaryShowcaseImage",
+                src: "images/hero_rmbrme.jpg",
+                alt: "image showing showcase of 'remember me' web app"
+            },
+        yearCreated: '2020',
+        titleOfProject: "rmbrme",
+        techUsed: 
+            [
+                "HTML5", 
+                "CSS 3", 
+                "ReactJS", 
+                "Node.js", 
+                "Express", 
+                "PostgreSQL"
+            ],
+        description: "This is for remembering stuff about people you care about. You create people and attached rmbrs (notes) to them. Rmbr is the branding name for a note about someone.",
+        buttons: [
+            {
+                title: "View Live",
+                link: "https://rmbrme.now.sh/"
+            },
+            {
+                title: "Server Repo",
+                link: "https://github.com/nick-nack-attack/rmbr-me-server"
+            },
+            {
+                title: "Client Repo",
+                link: "https://github.com/nick-nack-attack/rmbr-me-app"
+            }
+        ]
+    },
+    {
+        showcaseImage: 
+            {
+                id: "hero_pq",
+                class: "primaryShowcaseImage",
+                src: "images/hero_pineapple.jpg",
+                alt: "image showing showcase of Pineapple Quiz web app"
+            },
+        yearCreated: '2020',
+        titleOfProject: "The Finest Pineapple Quiz",
+        techUsed: 
+            [
+                "HTML", 
+                "CSS", 
+                "Javascript"
+            ],
+        description: "I'm working my way through the Thinkful Software engineering bootcamp. One of the assignments was to create a quiz. Mine is administered by a loquacious pineapple. However incorrigible this fruit may be, nay we forget its indescribable deliciousness.",
+        buttons: [
+            {
+                title: "View Live",
+                link: "https://nick-nack-attack.github.io/final-pineapple-quiz/"
+            },
+            {
+                title: "Repo",
+                link: "https://github.com/nick-nack-attack/final-pineapple-quiz.git"
+            }
+        ]
     }
 ]
